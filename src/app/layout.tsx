49b386c74dffe2e078575e6f0ec6398c0b21513b
Abdur Rohim <abdurrohimmm1122@gmail.com>
2026-02-21 03:57:26 +0000

    <meta name="pushsdk" content="f4e552425265a9e5267fd125510c6ec3">

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
        <head>
+        <meta name="pushsdk" content="f4e552425265a9e5267fd125510c6ec3" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
